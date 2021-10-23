cd2bf4d7c901ce8648b165ddfe7c1a82dbe349bb
Wellyngton-dev <contato@wcarvalho.dev>
2021-10-23 12:14:47 -0300

    app

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss'
+
+export function LoginBox() {
+    return (
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe sua mensagem</strong>
+            <a href="#" className={styles.signInWithGithub}>
+                Entrar com Github
+            </a>
+
+        </div>
+    )
+
+
+}
